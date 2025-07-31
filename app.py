4b30c2d254b4d43e12267d0a7387d91d8b6834a8
sruthiraman95Venkat <sruthiraman95@gmail.com>
2025-07-31 20:44:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello, this is a staging test website!</h1>"
+    return "<h1>Hello, this is a production website!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
